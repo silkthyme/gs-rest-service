11fa7e30607e5f8085a9cf75bd830695de82bee1
Christina Huang <huanchri@amazon.com>
2019-06-23 14:17:40 -0700

    create a resource controller

--- initial/src/main/java/hello/GreetingController.java
+++ initial/src/main/java/hello/GreetingController.java
@@ -7,3 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value ="name",
+	 defaultValue = "World") {
+		return new Greeting(counter.incrementAndGet(), 
+			String.format(template, name));
+	}
+}
