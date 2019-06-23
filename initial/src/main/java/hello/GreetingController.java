b6a3570347be484fd5e9f4babef7c5c71e75a1ef
Christina Huang <huanchri@amazon.com>
2019-06-23 14:24:30 -0700

    fix maven build error

--- initial/src/main/java/hello/GreetingController.java
+++ initial/src/main/java/hello/GreetingController.java
@@ -7,14 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-	
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value ="name",
-	 defaultValue = "World") {
-		return new Greeting(counter.incrementAndGet(), 
-			String.format(template, name));
-	}
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
 }
